983bc30fcbcb6538c257769af4b10a95ecc729ff
Colin McGraw <colinmcgraw@me.com>
2024-03-08 12:09:16 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Portfolio / Personal Website
+
+WIP
---
+++ astro.config.mjs
@@ -0,0 +1,27 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [mdx(), sitemap()],
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: 'poimandres',
+      // Alternatively, provide multiple themes
+      // https://github.com/antfu/shikiji#lightdark-dual-themes
+      experimentalThemes: {
+        light: 'github-light',
+        dark: 'github-dark',
+      },
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      langs: ['jinja-html'],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
+    },
+  }
+});
